883c117197aeedf8bdd8758d4088ebad88d3a8be
amitihere <amiti.aneesh1@gmail.com>
2026-02-09 22:20:26 +0530

    minor change

--- backend/src/catalog/routes.js
+++ backend/src/catalog/routes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const {validateFlightNumber} = require("../middleware/middle.js")
 const {get_flight_details,get_airport,get_flightsAiport,get_flightsAirlines,get_liveFlight} = require("../controller/controllers.js")
 const {signupMiddle,loginMiddle} = require("../middleware/userInfo.js")
-const {airportValidate,airportDetail,airlinesValidate, liveFlightValidate,} = require("../middleware/airport_middle.js")
+const {airportValidate,airportDetail,airlinesValidate} = require("../middleware/airport_middle.js")
 const {signupController,loginController} = require("../controller/userCredentials.js")
 
 router.get("/:flightNumber/:flightDate",validateFlightNumber,get_flight_details)
