6b3b99eca5909f9f349e4c3498436283680d015a
amitihere <amiti.aneesh1@gmail.com>
2026-02-11 10:38:22 +0530

    created new files for LOGIN/SINGUP

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useState } from 'react';
+
+const baseURL = import.meta.env.VITE_API_INFO;
+export default function Login(){
+    return (
+        <>
+            <div>
+                <h1>Login Page</h1>
+            </div>
+        </>
+    )
+}
