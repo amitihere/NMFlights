8fedc6e705471c4224634b9943695ba82bd32dd5
amitihere <amiti.aneesh1@gmail.com>
2026-02-12 12:21:21 +0530

    Add route for Airport Arrivals & Departures page
    - Import ArrivalsDepartures component
    - Add /arrivals-departures route to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Location from './pages/Location'
+import ArrivalsDepartures from './pages/ArrivalsDepartures'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/location" element={<Location />} />
+        <Route path="/arrivals-departures" element={<ArrivalsDepartures />} />
       </Routes>
     </>
   )
