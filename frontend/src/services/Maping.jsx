2ef73ba8808353023ac617518a9bb114e3c56594
amitihere <amiti.aneesh1@gmail.com>
2026-02-16 13:24:41 +0530

    modified the maping for good UI

--- frontend/src/services/Maping.jsx
+++ frontend/src/services/Maping.jsx
@@ -1,6 +1,8 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useEffect } from "react";
 import { useMap } from "react-leaflet";
+import axios from "axios";
+import './Maping.css'
 
 function ResizeFix() {
   const map = useMap();
@@ -15,25 +17,48 @@ function ResizeFix() {
 }
 
 export default function Maping() {
+  useEffect(()=> {
+    const loader = async () => {
+      try{
+          const respo = await axios.get(`http://localhost:3000/api/reqFlights/airports`)
+          console.log(respo.data)
+      }catch(err){
+        console.err(err)
+      }
+    }
+    loader()
+  },[])
+
   return (
-    <div style={{ height: "700px", width: "90%" ,marginTop: "50px",margin: "0 auto"}}>
-      <MapContainer
-        center={[28.6139, 77.2090]}
-        zoom={10}
-        style={{ height: "100%", width: "100%" }}
-      >
-        <ResizeFix />
+    <>
+      <div className="header">
+        <h1>Locate Flight on Maps</h1>
+        <p>
+          This section helps you track live flights on the map in real-time.
+          You can view exact positions, routes, and other important flight details
+          visually for better understanding.
+        </p>
+        <span style={{color:"red",margin:'10px'}}>Please search for the data of flight which is active</span>
+      </div>
 
-        <TileLayer
-          attribution='&copy; OpenStreetMap contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+      <div style={{ height: "700px", width: "90%" ,marginTop: "50px",margin: "0 auto"}}>
+        <MapContainer
+          center={[28.6139, 77.2090]}
+          zoom={10}
+          style={{ height: "100%", width: "100%" }}
+        >
+          <ResizeFix />
 
-        <Marker position={[28.6139, 77.2090]}>
-          <Popup>New Delhi</Popup>
-        </Marker>
-
-      </MapContainer>
-    </div>
+          <TileLayer
+            attribution='&copy; OpenStreetMap contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+      </div>
+      <div>
+        
+      </div>
+      
+    </>
   );
 }
