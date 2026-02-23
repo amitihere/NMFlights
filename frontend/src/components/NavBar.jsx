79454b996fba533bf287ba73d0f5f1ac6fd6b806
amitihere <amiti.aneesh1@gmail.com>
2026-02-24 01:20:28 +0530

    vercel sensi issues

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./Navbar.css";
+import "./NavBar.css";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
